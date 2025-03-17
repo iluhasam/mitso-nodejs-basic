9b5d74bf9cc7094f75540b108c2458f801fbb39f
bat9k <ilia34775@gmail.com>
2025-03-17 10:42:08 +0300

    feat: streams complete

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import path from "path";
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.resolve('src/streams/files/fileToRead.txt');
+    const stream = createReadStream(filePath, 'utf-8');
+    stream.pipe(process.stdout);
 };
 
 await read();
